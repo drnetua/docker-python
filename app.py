a10815acaa9b0dc244559e23c90c253714c7a1a0
gusa@cikulum.com <git5780057>
2018-07-29 16:44:34 +0300

    added quit endpoint

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-
-class User(Resource):
+class TestController(Resource):
     def get(self, param):
         response = {
             "status": "ok",
@@ -13,6 +12,11 @@ class User(Resource):
         }
         return response, 200
 
-api.add_resource(User, "/test/<string:param>")
+class QuitController(Resource):
+    def get(self):
+        sys.exit(0)
+
+api.add_resource(TestController, "/test/<string:param>")
+api.add_resource(QuitController, "/quit")
 
 app.run(host='0.0.0.0', debug=True)
